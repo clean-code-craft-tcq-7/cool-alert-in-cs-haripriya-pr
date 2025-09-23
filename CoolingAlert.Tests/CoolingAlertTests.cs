17b1ad96aabcb716643c0bfddc07034749fa9b40
haripriya-pr <prharipriya1998@gmail.com>
2025-09-23 16:49:09 +0530

    add code and tests for active cooling

--- CoolingAlert.Tests/CoolingAlertTests.cs
+++ CoolingAlert.Tests/CoolingAlertTests.cs
@@ -20,16 +20,16 @@ public class CoolingAlertTests
         Assert.Equal(ActionType.NO_ALERT, action.ActionType);
     }
     // ----------- STEP 2 code ends -------
-/*
+
     // ----------- STEP 4 -----------------
-    //[Fact]
-    //public void ActiveCoolingAlertToEmail()
-    //{
-    //    var action = CoolingAlert.BatteryTemperatureToAction(CoolingType.HI_ACTIVE_COOLING, 60.0f);
-    //    Assert.Equal(ActionType.ALERT_EMAIL, action.ActionType);
-    //    Assert.Equal("Temperature alert: 60.0F", action.ActionBody);
-    //}
+    [Fact]
+    public void ActiveCoolingAlertToEmail()
+    {
+        var action = CoolingAlert.BatteryTemperatureToAction(CoolingType.HI_ACTIVE_COOLING, 60.0f);
+        Assert.Equal(ActionType.ALERT_EMAIL, action.ActionType);
+        Assert.Equal("Temperature alert: 60.0F", action.ActionBody);
+    }
     // ----------- STEP 4 code ends -------
-*/
+
 }
 
